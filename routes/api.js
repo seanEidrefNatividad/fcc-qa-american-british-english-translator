e0b9564702fa1cb54bb62fe1d4f0faf287ce807c
seanEidrefNatividad <seaneidrefnatividad@gmail.com>
2025-02-27 16:57:12 +0800

    feat: /api/translate Post route completed

--- routes/api.js
+++ routes/api.js
@@ -8,6 +8,24 @@ module.exports = function (app) {
 
   app.route('/api/translate')
     .post((req, res) => {
+      const {locale, text} = req.body;
       
+      if (!locale || text === undefined) {res.send({ error: 'Required field(s) missing' });return;}
+      if (text.trim() === '') { res.send({ error: 'No text to translate' });return;}
+      if (locale !== 'american-to-british' && locale !== 'british-to-american') {res.send({ error: 'Invalid value for locale field' });return;}
+
+      const translatedText = translator.translate(locale,text)
+
+      if (text == translatedText) {
+        res.send({
+          text,
+          translation:'Everything looks good to me!'
+        });
+        return;
+      }
+      res.send({
+        text,
+        translation: translatedText
+      })
     });
 };
