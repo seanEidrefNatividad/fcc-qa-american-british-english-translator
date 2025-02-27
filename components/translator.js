518964d6cd065a6a904a1c18c984df8dfaecabc5
seanEidrefNatividad <seaneidrefnatividad@gmail.com>
2025-02-27 16:25:43 +0800

    feat: majority of  american to british translation and vice-versa logic completed

--- components/translator.js
+++ components/translator.js
@@ -11,16 +11,18 @@ class Translator {
     // capitalize the first word
     let currText = helper.capitalizeFirstLetter(text);
 
-    if (locale == 'american-to-british') {
+    // capitalize titles
+    const americanToBritishTitlesCapitalized =  helper.capitalizeTitles(americanToBritishTitles, locale);
 
-      // capitalize titles
-      const americanToBritishTitlesCapitalized =  helper.capitalizeTitles(americanToBritishTitles);
+    if (locale == 'american-to-british') {
 
       // merge objects
       const americanToBritish = {...americanToBritishTitlesCapitalized, ...americanOnly, ...americanToBritishSpelling}
 
+      //console.log(currText)
       // for loop replace words
-      currText = helper.replaceWords(currText, americanToBritish)
+      currText = helper.replaceWordsKeyValue(currText, americanToBritish)
+      currText = helper.replaceWordsValueKey(currText, britishOnly)
 
       // replace time
       currText = helper.replaceTime(currText, ':', '.')
@@ -29,6 +31,18 @@ class Translator {
       return currText;
     } else {
 
+      // merge objects
+      const americanToBritish = {...americanToBritishTitlesCapitalized, ...americanOnly, ...americanToBritishSpelling}
+
+      // for loop replace words
+      currText = helper.replaceWordsValueKey(currText, americanToBritish)
+      currText = helper.replaceWordsKeyValue(currText, britishOnly)
+
+      // replace time
+      currText = helper.replaceTime(currText, ':', '.')
+
+      // return data
+      return currText;
     }
   }
 }
@@ -36,19 +50,30 @@ class Helper {
   capitalizeFirstLetter(text) {
     return text[0].toUpperCase() + text.slice(1);
   } 
-  capitalizeTitles(titlesObject) {
+  capitalizeTitles(titlesObject, locale) {
     return Object.fromEntries(
-      Object.entries(titlesObject).map(([key, value]) => [
-        key,
-        this.capitalizeFirstLetter(value)
-      ])
+      Object.entries(titlesObject).map(([key, value]) => {
+        const newKey = locale == 'american-to-british' ? key : value;
+        const newValue = locale == 'american-to-british' ? value : key
+        return [
+        newKey,
+        this.capitalizeFirstLetter(newValue)
+      ]})
     );
   }
-  replaceWords(text, libraryOfWords) {
+  replaceWordsKeyValue(text, libraryOfWords) {
     let currText = text
     for (const [key, value] of Object.entries(libraryOfWords)) {
       let pattern = new RegExp(`\\b${key}\\b`, 'gi');
       currText = currText.replace(pattern, ` <span class="highlight">${value}</span> `) //white space important
+    }
+    return currText;
+  }
+  replaceWordsValueKey(text, libraryOfWords) {
+    let currText = text
+    for (const [key, value] of Object.entries(libraryOfWords)) {
+      let pattern = new RegExp(`\\b${value}\\b`, 'gi');
+      currText = currText.replace(pattern, ` <span class="highlight">${key}</span> `) //white space important
     }
     return currText;
   }
@@ -57,7 +82,6 @@ class Helper {
     let currentText = text.replace(pattern, `<span class="highlight">$1${delimiterAfter}$2</span>` )
     return currentText;
   }
-  
 }
 
 module.exports = Translator;
